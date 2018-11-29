c0353c9606d55bb0150df23c7ddc0ff923f7d659
WSHYD12 <WSHYD12@WSHYD-012>
2018-11-29 16:42:21 +0530

    3rd commit

--- Samplegithub/src/Spack/sclass.java
+++ Samplegithub/src/Spack/sclass.java
@@ -5,6 +5,7 @@ public class sclass {
 	public static void main(String[] args) {
 		System.out.println("Created project in Eclipse");
         System.out.println("Added 2ed line ");
+        System.out.println("Third line");
 	}
 
 }
